08fa5db678272720de8f4e9188dfcbaf5cfb7be7
nikita <ldc.endoscopy@gmail.com>
2024-09-23 22:30:32 +0400

    First commit

---
+++ medical_segmentation/segmentation/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+class ObjectClass(models.Model):
+    name = models.CharField(_('Name'), max_length=100)
+    description = models.TextField(_('Description'), blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class ImageUpload(models.Model):
+    image = models.ImageField(_('Image'), upload_to='uploads/')
+    object_class = models.ForeignKey(
+        ObjectClass, verbose_name=_('Object Class'),
+        on_delete=models.SET_NULL, null=True, blank=True
+        )
+    mask = models.ImageField(
+        _('Mask'), upload_to='masks/', blank=True, null=True
+        )
+    uploaded_at = models.DateTimeField(
+        _('Uploaded At'), auto_now_add=True
+        )
+
+    def __str__(self):
+        return f"Image {self.id} - {self.object_class or _('No class')}"
