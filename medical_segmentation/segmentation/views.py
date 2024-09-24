e62cc481a606ca9b13bc69a2cc44d0e45bc8ac5e
nikita <ldc.endoscopy@gmail.com>
2024-09-24 09:52:00 +0400

    Add internacionalization and dark theme

--- medical_segmentation/segmentation/views.py
+++ medical_segmentation/segmentation/views.py
@@ -3,6 +3,18 @@ from .forms import MultipleImageUploadForm
 from .models import ImageUpload
 
 def image_list(request):
+    if request.method == 'POST':
+        if 'delete_selected' in request.POST:
+            image_ids = request.POST.getlist('images')
+            for image_id in image_ids:
+                image = ImageUpload.objects.get(id=image_id)
+                image.delete()
+        elif 'delete_single' in request.POST:
+            image_id = request.POST.get('delete_single')
+            image = ImageUpload.objects.get(id=image_id)
+            image.delete()
+        return redirect('image_list')
+    
     images = ImageUpload.objects.all()
     return render(request, 'segmentation/image_list.html', {'images': images})
 
@@ -10,14 +22,13 @@ def upload_multiple_images(request):
     if request.method == 'POST':
         form = MultipleImageUploadForm(request.POST, request.FILES)
         if form.is_valid():
-            files = request.FILES.getlist('images')  # Получаем список всех загруженных файлов
-            object_class = form.cleaned_data['object_class']  # Получаем выбранный класс объекта
+            files = request.FILES.getlist('images')
+            object_class = form.cleaned_data['object_class']
             
-            # Проходим по каждому загруженному файлу и создаем отдельные записи
             for f in files:
                 ImageUpload.objects.create(image=f, object_class=object_class)
             
-            return redirect('image_list')  # Перенаправляем на список изображений
+            return redirect('image_list')
     else:
         form = MultipleImageUploadForm()
 
