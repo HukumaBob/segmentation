2e079657e0ed8f80467b853f8e3c45b8bda241f0
nikita <ldc.endoscopy@gmail.com>
2024-09-25 14:12:18 +0400

    Add models video, photo, mask

--- medical_segmentation/segmentation/models.py
+++ medical_segmentation/segmentation/models.py
@@ -30,3 +30,32 @@ class ImageUpload(models.Model):
 
     def __str__(self):
         return f"Image {self.id} - {self.object_class or _('No class')}"
+
+
+class Video(models.Model):
+    title = models.CharField(max_length=255, verbose_name="Название видео")
+    description = models.TextField(blank=True, verbose_name="Описание видео")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    video_file = models.FileField(upload_to='videos/', verbose_name="Файл видео")
+
+    def __str__(self):
+        return self.title
+
+
+class FrameSequence(models.Model):
+    video = models.ForeignKey(Video, related_name='frame_sequences', on_delete=models.CASCADE, verbose_name="Видео")
+    frame_file = models.ImageField(upload_to='frames/', verbose_name="Кадр")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+
+    def __str__(self):
+        return f"Кадр из {self.video.title} ({self.id})"
+
+
+class Mask(models.Model):
+    frame_sequence = models.ForeignKey(FrameSequence, related_name='masks', on_delete=models.CASCADE, verbose_name="Кадр")
+    mask_file = models.ImageField(upload_to='masks/', verbose_name="Файл маски")
+    tag = models.ForeignKey(ObjectClass, related_name='object_class', on_delete=models.CASCADE, verbose_name="Ярлык")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+
+    def __str__(self):
+        return f"Маска для кадра {self.frame_sequence.id} ({self.tag})"
