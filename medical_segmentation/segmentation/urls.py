0d00f895e7267e4da7365352b63ad3a9a455c845
nikita <ldc.endoscopy@gmail.com>
2024-10-12 22:36:14 +0400

    Video fix

--- medical_segmentation/segmentation/urls.py
+++ medical_segmentation/segmentation/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('upload-multiple/', views.upload_multiple_images, name='upload_multiple_images'),
     path('upload-video/', views.upload_video, name='upload_video'),
+    path('get_videos/', views.get_videos, name='get_videos'),
     path('delete-video/', views.delete_video, name='delete_video'),
     path('create-frame-sequence/<str:video_id>/', views.create_frame_sequence, name='create_frame_sequence'),
     path('view-frame-sequence/<str:video_id>/', views.view_frame_sequence, name='view_frame_sequence'),
